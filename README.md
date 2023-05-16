b4d94b3c1101a8ceff250e740593f82ee99d093e
Bernardo Sacuendi <bernardosacuendi@gmail.com>
2023-05-16 13:38:48 +0100

    inicialização do projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jogo CLÃ WAR
+## Trabalho da cadeira de progragamção 2 ISPTEC 2023
---
+++ src/cla_war/Personagem.java
@@ -0,0 +1,17 @@
+package cla_war;
+
+public class Personagem {
+    //energia vital
+    float ev;
+    //capacidade de dano
+    int cd;
+    //força de ataque
+    int fa;
+    //abilidade especial
+    int ab;
+    
+    //funçãoq que define a abilidade especiald e cada jogador com base no atributo ab
+    static void habilidadeEspecial(){
+        
+    }
+}
